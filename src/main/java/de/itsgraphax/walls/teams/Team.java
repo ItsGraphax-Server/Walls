3442150bac5cb1df19b4e5f20df39eebe32a8141
zegof <me@zegof.de>
2026-06-29 20:33:50 +0200

    Fixes #22: teamcolors not working after restart and adds teamcolors to the chat

--- src/main/java/de/itsgraphax/walls/teams/Team.java
+++ src/main/java/de/itsgraphax/walls/teams/Team.java
@@ -1,10 +1,12 @@
 package de.itsgraphax.walls.teams;
 
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.entity.Player;
 
 public class Team {
     private final int teamId;
     private final org.bukkit.scoreboard.Team bukkitTeam;
+    private final NamedTextColor teamColor;
 
     protected Team(TeamsManager teamsManager, int teamId) {
         org.bukkit.scoreboard.Team tempBukkitTeam;
@@ -16,13 +18,18 @@ public class Team {
         }
 
         this.bukkitTeam = tempBukkitTeam;
-        bukkitTeam.color(TeamsManager.getTeamTextColor(teamId));
+        this.teamColor = TeamsManager.getTeamTextColor(teamId);
+        bukkitTeam.color(teamColor);
     }
 
 
 
     public int teamId() {
         return teamId;
+    }
+
+    public NamedTextColor teamColor() {
+        return teamColor;
     }
 
     protected void addPlayer(Player player) {
